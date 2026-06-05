e88328aa5a0a244f882838301a413288258e5953
Gabriel Beaufils <gabriel.beaufils@etu.univ-lehavre.fr>
2026-06-05 16:29:22 +0200

    correction  Poisson

--- src/metier/Poisson.java
+++ src/metier/Poisson.java
@@ -12,8 +12,6 @@ public class Poisson
 
     private int        x;
     private int        y;
-    private int        indiceX;
-    private int        indiceY;
 
     public Poisson(String espece, int x, int y)
     {
