123c2c933501abc91616807b7178b9e43f2ca255
Gabriel Beaufils <gabriel.beaufils@etu.univ-lehavre.fr>
2026-06-08 09:42:49 +0200

    ajout de méthodes de sauvegarde + ajout de commentaires

--- src/metier/Metier.java
+++ src/metier/Metier.java
@@ -32,63 +32,64 @@ public class Metier
 
 
     public void genererLiaisons()
-{
-    this.lstLiaisons.clear();
-
-    // Réinitialisation de la grille des liaisons
-    for (int x = 0; x < grilleLiaisons.length; x++)
-        for (int y = 0; y < grilleLiaisons[x].length; y++)
-            grilleLiaisons[x][y] = '.';
-
-    // Collecte de tous les poissons présents dans la grille
-    List<Poisson> lstPoisson = new ArrayList<>();
-
-    for (int x = 0; x < grillePoisson.length; x++)
-        for (int y = 0; y < grillePoisson[x].length; y++)
-            if (grillePoisson[x][y] != null)
-                lstPoisson.add(grillePoisson[x][y]);
-
-    // Génération des liaisons
-    for (int i = 0; i < lstPoisson.size(); i++)
     {
-        for (int j = i + 1; j < lstPoisson.size(); j++)
+        this.lstLiaisons.clear();
+
+        // Réinitialisation de la grille des liaisons
+        for (int x = 0; x < grilleLiaisons.length; x++)
+            for (int y = 0; y < grilleLiaisons[x].length; y++)
+                grilleLiaisons[x][y] = '.';
+
+        // Collecte de tous les poissons présents dans la grille
+        List<Poisson> lstPoisson = new ArrayList<>();
+
+        for (int x = 0; x < grillePoisson.length; x++)
+            for (int y = 0; y < grillePoisson[x].length; y++)
+                if (grillePoisson[x][y] != null)
+                    lstPoisson.add(grillePoisson[x][y]);
+
+        // Génération des liaisons
+        for (int i = 0; i < lstPoisson.size(); i++)
         {
-            Poisson p1 = lstPoisson.get(i);
-            Poisson p2 = lstPoisson.get(j);
-
-            int dx = p2.getX() - p1.getX();
-            int dy = p2.getY() - p1.getY();
-
-            boolean alignes =
-                dx == 0 ||
-                dy == 0 ||
-                Math.abs(dx) == Math.abs(dy);
-
-            if (alignes && !existePoissonIntermediaire(p1, p2))
+            for (int j = i + 1; j < lstPoisson.size(); j++)
             {
-                Liaison l = new Liaison(p1, p2);
-                this.lstLiaisons.add(l);
+                Poisson p1 = lstPoisson.get(i);
+                Poisson p2 = lstPoisson.get(j);
 
-                // Marquage des cases traversées
-                int pasX = Integer.signum(dx);
-                int pasY = Integer.signum(dy);
+                int dx = p2.getX() - p1.getX();
+                int dy = p2.getY() - p1.getY();
 
-                int x = p1.getX() + pasX;
-                int y = p1.getY() + pasY;
+                boolean alignes =
+                    dx == 0 ||
+                    dy == 0 ||
+                    Math.abs(dx) == Math.abs(dy);
 
-                while (x != p2.getX() || y != p2.getY())
+                if (alignes && !existePoissonIntermediaire(p1, p2))
                 {
-                    // On ne marque que les cases intermédiaires
-                    grilleLiaisons[x][y] = '*';
+                    Liaison l = new Liaison(p1, p2);
+                    this.lstLiaisons.add(l);
 
-                    x += pasX;
-                    y += pasY;
+                    // Marquage des cases traversées
+                    int pasX = Integer.signum(dx);
+                    int pasY = Integer.signum(dy);
+
+                    int x = p1.getX() + pasX;
+                    int y = p1.getY() + pasY;
+
+                    while (x != p2.getX() || y != p2.getY())
+                    {
+                        // On ne marque que les cases intermédiaires
+                        grilleLiaisons[x][y] = '*';
+
+                        x += pasX;
+                        y += pasY;
+                    }
                 }
             }
         }
     }
-}
 
+    // Méthode pour vérifier s'il existe un poisson intermédiaire entre deux poissons alignés
 
     private boolean existePoissonIntermediaire( Poisson p1, Poisson p2 )
     {
@@ -107,21 +108,7 @@ public class Metier
         return false;
     }
 
-
-    public Poisson getPoisson( int x, int y )
-    {
-        return this.grillePoisson[x][y];
-    }
-
-    public Poisson[][] getGrillePoisson()
-    {
-        return this.grillePoisson;
-    }
-
-    public List<Poisson> getLstPoisson()
-    {
-        return this.lstPoisson;
-    }
+    // Méthode pour positionner un poisson à une position donnée
 
     public void positionPoisson( int x, int y, String espece)
     {
@@ -130,6 +117,28 @@ public class Metier
         this.lstPoisson.add( p );
     }
 
+    // Méthodes pour obtenir un poisson à une position donnée et pour obtenir la grille des poissons
+
+    public Poisson getPoisson( int x, int y )
+    {
+        return this.grillePoisson[x][y];
+    }
+
+    // Méthode pour obtenir la grille des poissons
+
+    public Poisson[][] getGrillePoisson()
+    {
+        return this.grillePoisson;
+    }
+
+    // Méthode pour obtenir la liste de tous les poissons
+
+    public List<Poisson> getLstPoisson()
+    {
+        return this.lstPoisson;
+    }
+
+    // Méthode pour supprimer un poisson à une position donnée
 
     public void supprimerPoisson( int x, int y )
     {
@@ -215,7 +224,6 @@ public class Metier
         }
     }
 
-
     public void sauvegarderPoissons( List<Poisson> lstPoisson )
     {
         try (BufferedWriter writer = new BufferedWriter( new FileWriter( "poissons.txt" ) ))
@@ -223,6 +231,46 @@ public class Metier
             for ( Poisson p : lstPoisson )
             {
                 writer.write( p.getEspece() + " " + p.getX() + " " + p.getY() );
+                writer.newLine();
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void sauvegarderZones( Zone[][] grilleZone )
+    {
+        try (BufferedWriter writer = new BufferedWriter( new FileWriter( "zones.txt" ) ))
+        {
+            for ( int y = 0; y < grilleZone[0].length; y++ )
+            {
+                for ( int x = 0; x < grilleZone.length; x++ )
+                {
+                    if ( grilleZone[x][y] != null )
+                    {
+                        writer.write( grilleZone[x][y].getNumZone() + " " );
+                    } else
+                    {
+                        writer.write( ". " );
+                    }
+                }
+                writer.newLine();
+            }
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void sauvegarderLiaisons( List<Liaison> lstLiaisons )
+    {
+        try (BufferedWriter writer = new BufferedWriter( new FileWriter( "liaisons.txt" ) ))
+        {
+            for ( Liaison l : lstLiaisons )
+            {
+                writer.write( l.getP1().getEspece() + " (" + l.getP1().getX() + ", " + l.getP1().getY() + ") <-> "
+                    + l.getP2().getEspece() + " (" + l.getP2().getX() + ", " + l.getP2().getY() + ")" );
                 writer.newLine();
             }
         } catch (IOException e)
