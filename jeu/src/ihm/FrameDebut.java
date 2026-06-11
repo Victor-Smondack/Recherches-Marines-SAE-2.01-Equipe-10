ec5b8ec59da13547aec6c475f7a60c51d5c2faec
AlexisBfy <alexis.bouffay@etu.univ-lehavre.fr>
2026-06-11 13:57:33 +0200

    Ajout de FrameDebut

---
+++ jeu/src/ihm/FrameDebut.java
@@ -0,0 +1,23 @@
+package src.ihm;
+
+import src.Controleur;
+
+public class FrameDebut extends JFrame
+{
+    private Controleur ctrl;
+    private PanelDebut panelDebut;
+
+    public FrameDebut ( Controleur ctrl)
+    {
+        this.ctrl = ctrl;
+
+        this.setTitle   ( "Menu du jeu" );
+		this.setSize    ( 250,500 );
+		this.setLocation( 20, 50 );
+
+        this.panelDebut = new PanelDebut( ctrl );
+
+        this.add( this.panelDebut );
+        this.setVisible( true );
+    }
+}
