3f496c029bedddafb8c87d4f2ee0e7cde55d2594
AlexisBfy <alexis.bouffay@etu.univ-lehavre.fr>
2026-06-11 14:12:24 +0200

    Ajout de PanelDebut et d'un bouton

---
+++ jeu/src/ihm/PanelDebut.java
@@ -0,0 +1,31 @@
+package src.ihm;
+
+import src.Controleur;
+
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+
+import java.awt.event.*;
+
+public class PanelDebut extends JPanel implements ActionListener
+{
+    private Controleur ctrl;
+
+    private JButton    btnValider;
+
+    public PanelDebut ( Controleur ctrl )
+    {
+        this.ctrl = ctrl;
+
+        this.setLayout( new BorderLayout( ) );
+
+        //Création des composants
+
+        this.btnValider = new JButton( "Valider" );
+
+        //Position des Composants
+
+        this.add( btnValider, BorderLayout.NORTH );
+    }
+}
